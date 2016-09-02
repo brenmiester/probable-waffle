a53c5fe7aff843be91d3fba223a2ff54ded7a720
brenmiester <samuelsson.benjamin01@gmail.com>
2016-09-02 11:54:37 +0800

    Updated run.js
    Removed the attack modules, entity modules, and built the query/invcheck modules.

--- run.js
+++ run.js
@@ -9,17 +9,6 @@
  cs9 = ' '
  cs10 = ' '
  damage = 0
- usr = ' '
- usr.charname = ' '
- usr.class = ' '
- usr.spells = false
- usr.health = 20
- usr.maxhealth = 40
- usr.sucessrate = 0.2
- entity = "Ork"
- entity.health = 15
- entity.attack = 5
- entity.alive = True
  function addtext() {
   add = document.getElementById("addtext").value
   cs1 = cs2;
@@ -44,25 +33,9 @@
   document.getElementById('cs10').innerHTML = cs10;
   console.log(add + ' was printed');
  }
- function attack(damage) {
- 	entity.health = entity.health - damage
- 	if entity.health < 0
- 		entity.alive = false
- 	else 
- 	usrattack()
- }
-  function usrattack(damage) {
- 	usr.health = usr.health - damage
- 	if usr.health < 0
- 		endgame()
- 	else 
- 		attackprompt()
- }
- function spcast(spell){
-  if spell = "Fireball"
-   var rndtest = math.random()
-   if rndtest > successrate
-    health = health - 15
-   else
-    attack(15)
+ function query(q){
+  add(q)
+  return document.getElementById('txt').value
+  if executed = 0
+   
  }
